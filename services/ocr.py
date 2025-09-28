9269a0c79d4fa37ae2732afae36fd3629ecafe2a
Amritanshu Keshari <84897578+AnshuWorld@users.noreply.github.com>
2025-09-28 17:23:38 +0530

    Update ocr.py

--- services/ocr.py
+++ services/ocr.py
@@ -1,10 +1,11 @@
 import pytesseract
 from PIL import Image
 import io
+from services.utils import safe_decode
 
 def extract_text(file, content):
     try:
         image = Image.open(io.BytesIO(content))
         return pytesseract.image_to_string(image, lang="eng+mal")
     except Exception:
-        return content.decode("utf-8", errors="ignore")
+        return safe_decode(content)
